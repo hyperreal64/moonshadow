d47407c05bd93f07c99e54aeec95f6792f9d3010
Jeffrey Serio <hyperreal@moonshadow.dev>
2026-08-22 11:36:57 -0500

    Add VSCode extensions

--- system_files/usr/share/ublue-os/user-setup.hooks.d/11-vscode-extensions.sh
+++ system_files/usr/share/ublue-os/user-setup.hooks.d/11-vscode-extensions.sh
@@ -6,11 +6,27 @@ version-script vscode-extensions-dx user 1 || exit 1
 
 set -x
 
-# Setup VSCodium
-if test ! -e "$HOME"/.config/Codium/User/settings.json; then
-	mkdir -p "$HOME"/.config/Codium/User
-	cp -f /etc/skel/.config/Codium/User/settings.json "$HOME"/.config/Codium/User/settings.json
+# Setup VSCode
+if test ! -e "$HOME"/.config/Code/User/settings.json; then
+	mkdir -p "$HOME"/.config/Code/User
+	cp -f /etc/skel/.config/Code/User/settings.json "$HOME"/.config/Code/User/settings.json
 fi
 
-codium --install-extension ms-vscode-remote.remote-containers
-codium --install-extension ms-vscode-remote.remote-ssh
+code --install-extension Catppuccin.catppuccin-vsc
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension foxundermoon.shell-format
+code --install-extension mads-hartmann.bash-ide-vscode
+code --install-extension ms-azuretools.vscode-containers
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode-remote.remote-containers
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension njpwerner.autodocstring
+code --install-extension njqdev.vscode-python-typehint
+code --install-extension redhat.vscode-xml
+code --install-extension redhat.vscode-yaml
+code --install-extension stylelint.vscode-stylelint
+code --install-extension timonwong.shellcheck
+code --install-extension yzhang.markdown-all-in-one
