a93b28518ff721853dde9f4cbea6b2cccb345d47
Jeffrey Serio <hyperreal@moonshadow.dev>
2026-08-22 02:49:39 -0500

    Update

--- Containerfile
+++ Containerfile
@@ -1,15 +1,14 @@
+# Allow build scripts to be referenced without being copied into the final image
 FROM scratch AS ctx
-COPY build_files /build_files
-COPY system_files /files
+COPY build_files /
+COPY system_files /system_files
 
-FROM ghcr.io/ublue-os/bazzite-nvidia:stable
+FROM ghcr.io/ublue-os/bazzite-dx-nvidia:stable
 
-ARG IMAGE_NAME="moonshadow"
-ARG IMAGE_VENDOR="hyperreal64"
-
-RUN --mount=type=tmpfs,dst=/tmp \
-    --mount=type=bind,from=ctx,source=/,target=/run/context \
-    mkdir -p /var/roothome && \
-    /run/context/build_files/build.sh
+RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
+    --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /ctx/build.sh
 
 RUN bootc container lint
--- build_files/build.sh
+++ build_files/build.sh
@@ -1,44 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -eo pipefail
+set -ouex pipefail
 
-CONTEXT_PATH="$(realpath "$(dirname "$0")/..")" # should return /run/context
-BUILD_SCRIPTS_PATH="$(realpath "$(dirname $0)")"
-MAJOR_VERSION_NUMBER="$(sh -c '. /usr/lib/os-release ; echo $VERSION_ID')"
-SCRIPTS_PATH="$(realpath "$(dirname "$0")/scripts")"
-export CONTEXT_PATH
-export SCRIPTS_PATH
-export MAJOR_VERSION_NUMBER
+# Copy the contents of system_files/ of the git repo to /
+cp -avf "/ctx/system_files"/. /
 
-run_buildscripts_for() {
-	WHAT=$1
-	shift
-	# Complex "find" expression here since there might not be any overrides
-	# Allows us to numerically sort scripts by stuff like "01-packages.sh" or whatever
-	# CUSTOM_NAME is required if we dont need or want the automatic name
-	find "${BUILD_SCRIPTS_PATH}/$WHAT" -maxdepth 1 -iname "*-*.sh" -type f -print0 | sort --zero-terminated --sort=human-numeric | while IFS= read -r -d $'\0' script; do
-		if [ "${CUSTOM_NAME}" != "" ]; then
-			WHAT=$CUSTOM_NAME
-		fi
-		printf "::group:: ===$WHAT-%s===\n" "$(basename "$script")"
-		"$(realpath $script)"
-		printf "::endgroup::\n"
-	done
-}
+# Additional packages
+dnf5 install -y \
+	atop \
+	nfs-utils \
+	prometheus-node-exporter \
+	rpcbind \
+	yakuake
 
-copy_systemfiles_for() {
-	WHAT=$1
-	shift
-	DISPLAY_NAME=$WHAT
-	if [ "${CUSTOM_NAME}" != "" ]; then
-		DISPLAY_NAME=$CUSTOM_NAME
-	fi
-	printf "::group:: ===%s-file-copying===\n" "${DISPLAY_NAME}"
-	cp -avf "${CONTEXT_PATH}/$WHAT/." /
-	printf "::endgroup::\n"
-}
-
-CUSTOM_NAME="base"
-copy_systemfiles_for files
-run_buildscripts_for .
-CUSTOM_NAME=
+# systemd services
+systemctl enable atop.service
+systemctl enable atopacct.service
+systemctl enable atop-rotate.timer
+systemctl enable prometheus-node-exporter.service
+systemctl enable rpcbind.service
+systemctl disable cups.service
